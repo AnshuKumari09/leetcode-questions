8032d14de5c09cf18653f0cc15f8934482a435c8
AnshuKumari09 <iamanshu6920@gmail.com>
2026-01-11 12:32:56 +0530

    Time: 4 ms (11.59%) | Memory: 8.8 MB (30.87%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
@@ -1,3 +1,38 @@
+// class Solution {
+// public:
+//     int countGoodSubstrings(string s) {
+//         unordered_map<char,int>mp;
+//         for(int i=0;i<3;i++){
+//             mp[s[i]]++;
+//         }
+//         int i=0;
+//         int j=2;
+//         int n=s.length();
+//         int result=0;
+//         while(j<n){
+//             int count=0;
+//             for(auto m:mp){
+//                 if(mp[m.first]==1){
+//                     count++;
+//                 }
+//             }
+//             if(count==3){
+//                 result++;
+//             }
+
+//             if(mp[s[i]]>0){
+//                  mp[s[i]]-=1;
+//             }
+//             i++;
+//             j++;
+//             if(j<n){
+//                 mp[s[j]]++;
+//             }
+//         }
+//         return result;
+//     }
+// };
+
 class Solution {
 public:
     int countGoodSubstrings(string s) {
@@ -10,18 +45,12 @@ public:
         int n=s.length();
         int result=0;
         while(j<n){
-            int count=0;
-            for(auto m:mp){
-                if(mp[m.first]==1){
-                    count++;
-                }
-            }
-            if(count==3){
+            if(mp.size()==3){
                 result++;
             }
-
-            if(mp[s[i]]>0){
-                 mp[s[i]]-=1;
+            mp[s[i]]--;
+            if(mp[s[i]]==0){
+                 mp.erase(s[i]);
             }
             i++;
             j++;
