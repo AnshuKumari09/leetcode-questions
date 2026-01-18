25a3c4f5321a3e620fac698cfe8e31e904c3653b
AnshuKumari09 <iamanshu6920@gmail.com>
2026-01-18 10:17:00 +0530

    Time: 4 ms (9.29%) | Memory: 23.5 MB (6.29%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,43 @@
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         if (nums.empty()) return 0;
+
+//         int index = 1; // Position to insert the next unique element
+
+//         for (int i = 1; i < nums.size(); i++) {
+//             if (nums[i] != nums[i - 1]) {
+//                 nums[index] = nums[i];
+//                 index++;
+//             }
+//         }
+
+//         return index;
+//     }
+// };
+
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        set<int>visited;
+        int n=nums.size();
+        if(n==0){
+            return 0;
+        }
+         int i=0;
+         int j=0;
+        while(j<n){
+            if(visited.count(nums[j])==0){ // j visited nhi hai
+               // visited mark karo
+               visited.insert(nums[j]);
+               nums[i]=nums[j];
+               i++;
+            }
+            j++;
+        }
+       // ffill the place with spaces
+      
+       return i;
+    }
+};
