46f99a79288489cf05838f5b5ef0cf5a7f66f486
AnshuKumari09 <iamanshu6920@gmail.com>
2026-04-13 07:20:16 +0530

    Time: 3 ms (64.90%) | Memory: 18.1 MB (45.12%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/top-k-frequent-words.cpp
@@ -1,3 +1,37 @@
+// class Solution {
+// public:
+//     vector<string> topKFrequent(vector<string>& words, int k) {
+//         typedef pair<int,string>P;
+//         struct comp{
+//             bool operator()(P& a,P& b){
+//                 if(a.first==b.first){
+//                     return a.second>b.second;
+//                 }
+//                 else{
+//                     return b.first>a.first;
+//                 }
+//             }
+//         };
+//         unordered_map<string,int>mp;
+//         for(string word:words){
+//             mp[word]++;
+//         }
+//         priority_queue<P,vector<P>,comp>pq;
+//         for(auto m:mp){
+//              pq.push({m.second,m.first});  
+//         }
+//         vector<string>result;
+
+//         while(k--){
+//             result.push_back(pq.top().second);
+//             pq.pop();
+            
+//         }
+//         return result;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
@@ -5,10 +39,10 @@ public:
         struct comp{
             bool operator()(P& a,P& b){
                 if(a.first==b.first){
-                    return a.second>b.second;
+                    return a.second<b.second;
                 }
                 else{
-                    return b.first>a.first;
+                    return b.first<a.first;
                 }
             }
         };
@@ -19,14 +53,17 @@ public:
         priority_queue<P,vector<P>,comp>pq;
         for(auto m:mp){
              pq.push({m.second,m.first});  
+             if(pq.size()>k){
+                pq.pop();
+             }
         }
         vector<string>result;
 
         while(k--){
             result.push_back(pq.top().second);
             pq.pop();
-            
         }
+        reverse(result.begin(),result.end());
         return result;
     }
 };
