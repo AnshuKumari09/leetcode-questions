63279be1e4148bb9a69375655c3ac404cf565859
AnshuKumari09 <iamanshu6920@gmail.com>
2026-05-15 07:08:09 +0530

    Time: 11 ms (41.42%) | Memory: 18.9 MB (26.33%) - LeetSync

---
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        vector<string>result;
+        for(string query:queries){
+               for(string dict:dictionary){
+                int i=0;
+                int countNotMatching=0;
+                while(i<=query.length()-1){
+                    
+                    if(query[i]!=dict[i]){
+                        countNotMatching++;
+                    }
+                    if(countNotMatching>2 || query.length()!=dict.length()){
+                        break;
+                    }
+                    i++;
+                }
+                 if(countNotMatching<=2){
+                  result.push_back(query);
+                  break;
+               }
+               }
+              
+        }
+        return result;
+    }
+};
