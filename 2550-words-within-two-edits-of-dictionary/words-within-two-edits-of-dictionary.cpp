ed004738f801066b82588c8243bf86ae87263c7a
AnshuKumari09 <iamanshu6920@gmail.com>
2026-05-15 07:10:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (64.37%) - LeetSync

--- 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.cpp
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.cpp
@@ -1,28 +1,69 @@
+// class Solution {
+// public:
+//     vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+//         vector<string>result;
+//         for(string query:queries){
+//                for(string dict:dictionary){
+//                 int i=0;
+//                 int countNotMatching=0;
+//                 while(i<=query.length()-1){
+                    
+//                     if(query[i]!=dict[i]){
+//                         countNotMatching++;
+//                     }
+//                     if(countNotMatching>2){
+//                         break;
+//                     }
+//                     i++;
+//                 }
+//                  if(countNotMatching<=2){
+//                   result.push_back(query);
+//                   break;
+//                }
+//                }
+              
+//         }
+//         return result;
+//     }
+// };
+
+
 class Solution {
 public:
-    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
-        vector<string>result;
-        for(string query:queries){
-               for(string dict:dictionary){
-                int i=0;
-                int countNotMatching=0;
-                while(i<=query.length()-1){
-                    
-                    if(query[i]!=dict[i]){
-                        countNotMatching++;
-                    }
-                    if(countNotMatching>2){
-                        break;
-                    }
-                    i++;
-                }
-                 if(countNotMatching<=2){
-                  result.push_back(query);
-                  break;
-               }
-               }
-              
+
+    bool isPossible(string &a, string &b){
+
+        int mismatch = 0;
+
+        for(int i = 0; i < a.length(); i++){
+
+            if(a[i] != b[i]){
+                mismatch++;
+            }
+
+            if(mismatch > 2){
+                return false;
+            }
         }
+
+        return true;
+    }
+
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+
+        vector<string> result;
+
+        for(string &query : queries){
+
+            for(string &dict : dictionary){
+
+                if(isPossible(query, dict)){
+                    result.push_back(query);
+                    break;
+                }
+            }
+        }
+
         return result;
     }
 };
