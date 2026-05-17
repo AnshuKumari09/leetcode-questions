3f13969f027e6bc43486e4a67ac7b6b1d962f9c0
AnshuKumari09 <iamanshu6920@gmail.com>
2026-05-17 22:40:19 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (17.78%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,67 +1,30 @@
-// class Solution {
-// public:
-//     int removeDuplicates(vector<int>& nums) {
-//         if (nums.empty()) return 0;
-
-//         int index = 1; // Position to insert the next unique element
-
-//         for (int i = 1; i < nums.size(); i++) {
-//             if (nums[i] != nums[i - 1]) {
-//                 nums[index] = nums[i];
-//                 index++;
-//             }
-//         }
-
-//         return index;
-//     }
-// };
-
-
-// class Solution {
-// public:
-//     int removeDuplicates(vector<int>& nums) {
-//         set<int>visited;
-//         int n=nums.size();
-//         if(n==0){
-//             return 0;
-//         }
-//          int i=0;
-//          int j=0;
-//         while(j<n){
-//             if(visited.count(nums[j])==0){ // j visited nhi hai
-//                // visited mark karo
-//                visited.insert(nums[j]);
-//                nums[i]=nums[j];
-//                i++;
-//             }
-//             j++;
-//         }
-//        // ffill the place with spaces
-      
-//        return i;
-//     }
-// };
-
-
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-       
         int n=nums.size();
-        if(n==0){
-            return 0;
-        }
-      int i=0;
-      
-      for(int j=1;j<n;j++){
-          if(nums[i]!=nums[j]){
-            i++;
-            nums[i]=nums[j];
+        int i=0;
+        int count=1;
+        while(i<n-1){
+            int num=nums[i];
+            bool found=false;
+            for(int j=i+1;j<n;j++){
+                if(nums[j]!=num){
+                    found=true;
+                    nums[count]=nums[j];
+                    i=j;
+                    count++;
+                   break;
+
+                }
+               
+            }
+            if(found==false){
+                break;
+            }
+
              
-          }
-        
-      }
-      
-       return i+1;
+
+        }
+        return count;
     }
 };
