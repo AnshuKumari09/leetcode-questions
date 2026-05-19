5bf0153e6acf86e14a140946f2aa102c951c1bce
AnshuKumari09 <iamanshu6920@gmail.com>
2026-05-19 22:11:46 +0530

    Time: 0 ms (100.00%) | Memory: 124.3 MB (66.96%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -31,27 +31,66 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int pairSum(ListNode* head) {
+//         ListNode* temp=head;
+//         ListNode* temp1=head;
+//         stack<int>res;
+//         int n=0;
+
+//         while(temp){
+//             n++;
+//             res.push(temp->val);
+//             temp=temp->next;
+            
+//         }
+//         int maxS=0;
+//         for(int i=0;i<n/2;i++){
+//             int sum=res.top()+temp1->val;
+//             res.pop();
+//             maxS=max(maxS,sum);
+//             temp1=temp1->next;3
+//         }
+//      return maxS;
+//     }
+// };
+
+
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        ListNode* temp=head;
-        ListNode* temp1=head;
-        stack<int>res;
-        int n=0;
+      // third approach 
+    //   a-b-c-d-e-f
+    //   a-b-c-f-e-d  -> mid pointer at f . okay ab kya akre
+    //   pehle mujhe mid tk jana hoga . slow aur fats lga do
+      ListNode* slow=head;
+      ListNode* temp=head;
+      ListNode* fast=head;
+      while(fast && fast->next){
+         slow=slow->next;
+         fast=fast->next->next;
+      }
+    //   list mil gyi ab reverse krna h. 
 
-        while(temp){
-            n++;
-            res.push(temp->val);
-            temp=temp->next;
-            
-        }
-        int maxS=0;
-        for(int i=0;i<n/2;i++){
-            int sum=res.top()+temp1->val;
-            res.pop();
-            maxS=max(maxS,sum);
-            temp1=temp1->next;
-        }
-     return maxS;
+      ListNode* mid=slow;
+      ListNode* prev=NULL;
+      ListNode* curr=slow;
+      while(curr){
+           ListNode* future=curr->next;
+           curr->next=prev;
+           prev=curr;
+           curr=future;
+      }
+
+      //concatenate 
+      int maxValue=0;
+         while(prev){
+            int value=prev->val + temp->val;
+              maxValue=max(maxValue,value);
+              prev=prev->next;
+              temp=temp->next;
+         }
+       return maxValue;
     }
 };
