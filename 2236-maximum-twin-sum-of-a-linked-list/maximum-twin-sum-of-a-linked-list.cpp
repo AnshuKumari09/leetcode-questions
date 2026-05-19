b5007c61dffc806b2305148601751eca159c1303
AnshuKumari09 <iamanshu6920@gmail.com>
2026-05-19 16:54:43 +0530

    Time: 14 ms (7.77%) | Memory: 134.2 MB (27.20%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -8,25 +8,50 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     int pairSum(ListNode* head) {
+//         ListNode* temp=head;
+//         vector<int>res;
+//         while(temp){
+//             res.push_back(temp->val);
+//             temp=temp->next;
+            
+//         }
+//         int i=0;
+//         int j=res.size()-1;
+//         int maxS=0;
+//        while(i<j){
+//           int sum=res[i]+res[j];
+//           maxS=max(maxS,sum);
+//           j--;
+//           i++;
+//        }
+//        return maxS;
+//     }
+// };
+
 class Solution {
 public:
     int pairSum(ListNode* head) {
         ListNode* temp=head;
-        vector<int>res;
+        ListNode* temp1=head;
+        stack<int>res;
+        int n=0;
+
         while(temp){
-            res.push_back(temp->val);
+            n++;
+            res.push(temp->val);
             temp=temp->next;
             
         }
-        int i=0;
-        int j=res.size()-1;
         int maxS=0;
-       while(i<j){
-          int sum=res[i]+res[j];
-          maxS=max(maxS,sum);
-          j--;
-          i++;
-       }
-       return maxS;
+        for(int i=0;i<n/2;i++){
+            int sum=res.top()+temp1->val;
+            res.pop();
+            maxS=max(maxS,sum);
+            temp1=temp1->next;
+        }
+     return maxS;
     }
 };
