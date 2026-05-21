6e43bcc5657abdb6e5dd462bb678a5bc7c774ea6
AnshuKumari09 <iamanshu6920@gmail.com>
2026-05-21 12:19:04 +0530

    Time: 0 ms (100.00%) | Memory: 63.7 MB (12.53%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,46 +1,3 @@
-// class Solution {
-// public:
-//     int longestSubarray(vector<int>& nums) {
-//        int n=nums.size();
-//        vector<int>vec(n);
-//        int num=n;
-//        for(int i=n-1;i>=0;i--){
-//           vec[i]=num;
-//           if(nums[i]==0){
-//             num=i;
-//           }
-//        } 
-
-//        int i=0;
-//        int j=0;
-//        int countZero=0;
-//        int maxLength=0;
-
-//        while(j<n){
-//           if(nums[j]==0){
-//              countZero++;
-//           }
-//           if(countZero==1){
-//              int length=vec[j]-i;
-             
-//              maxLength=max(maxLength,length);
-//           }
-//           while(countZero>1){
-//             if(nums[i]==0){
-// countZero--;
-//             }
-            
-//             i++;
-//           }
-            
-//            maxLength=max(maxLength,j-i);
-//         j++;
-//        }
-// return maxLength;
-//     }
-// };
-
-
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
@@ -63,7 +20,11 @@ public:
           if(nums[j]==0){
              countZero++;
           }
-          
+          if(countZero==1){
+             int length=vec[j]-i-1;
+             
+             maxLength=max(maxLength,length);
+          }
           while(countZero>1){
             if(nums[i]==0){
 countZero--;
@@ -78,3 +39,42 @@ countZero--;
 return maxLength;
     }
 };
+
+
+// class Solution {
+// public:
+//     int longestSubarray(vector<int>& nums) {
+//        int n=nums.size();
+//        vector<int>vec(n);
+//        int num=n;
+//        for(int i=n-1;i>=0;i--){
+//           vec[i]=num;
+//           if(nums[i]==0){
+//             num=i;
+//           }
+//        } 
+
+//        int i=0;
+//        int j=0;
+//        int countZero=0;
+//        int maxLength=0;
+
+//        while(j<n){
+//           if(nums[j]==0){
+//              countZero++;
+//           }
+          
+//           while(countZero>1){
+//             if(nums[i]==0){
+// countZero--;
+//             }
+            
+//             i++;
+//           }
+            
+//            maxLength=max(maxLength,j-i);
+//         j++;
+//        }
+// return maxLength;
+//     }
+// };
