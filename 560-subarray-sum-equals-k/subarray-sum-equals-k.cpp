e723b626311fe91823f19229da4760b1d8d4d499
AnshuKumari09 <iamanshu6920@gmail.com>
2026-05-25 19:01:49 +0530

    Time: 2497 ms (9.20%) | Memory: 34.4 MB (82.58%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -31,27 +31,27 @@
 //     }
 // };
 
-// class Solution {
-// public:
-//     int subarraySum(vector<int>& nums, int k) {
-//         // with two pointer method
-//         int n=nums.size();
-//         int i=0;
-//         int j=0;
-//         int res=0;
-//         int sum=0;
-//        for(int i=0;i<n;i++){
-//         int sum=0;
-//         for(int j=i;j<n;j++){
-//              sum+=nums[j];
-//              if(sum==k){
-//                res++;
-//              }
-//         }
-//        }
-//         return res;
-//     }
-// };
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        // with two pointer method
+        int n=nums.size();
+        int i=0;
+        int j=0;
+        int res=0;
+        int sum=0;
+       for(int i=0;i<n;i++){
+        int sum=0;
+        for(int j=i;j<n;j++){
+             sum+=nums[j];
+             if(sum==k){
+               res++;
+             }
+        }
+       }
+        return res;
+    }
+};
 
 
 
@@ -111,40 +111,40 @@
 // };
 
 
-class Solution {
-public:
-    int subarraySum(vector<int>& nums, int k) {
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
 
-        int n = nums.size();
+//         int n = nums.size();
 
-        int sum = 0;
+//         int sum = 0;
 
-        vector<int> vec;
+//         vector<int> vec;
 
-        for(int i = 0; i < n; i++) {
-            sum += nums[i];
-            vec.push_back(sum);
-        }
+//         for(int i = 0; i < n; i++) {
+//             sum += nums[i];
+//             vec.push_back(sum);
+//         }
 
-        int res = 0;
+//         int res = 0;
 
-        unordered_map<int,int> mp;
+//         unordered_map<int,int> mp;
 
-        mp[0] = 1;
+//         mp[0] = 1;
 
-        for(int i = 0; i < n; i++) {
+//         for(int i = 0; i < n; i++) {
 
-            int num = vec[i] - k;
+//             int num = vec[i] - k;
 
-            // agar pehle mila hai
-            if(mp.find(num) != mp.end()) {
-                res += mp[num];
-            }
+//             // agar pehle mila hai
+//             if(mp.find(num) != mp.end()) {
+//                 res += mp[num];
+//             }
 
-            // current prefix sum store karo
-            mp[vec[i]]++;
-        }
+//             // current prefix sum store karo
+//             mp[vec[i]]++;
+//         }
 
-        return res;
-    }
-};
+//         return res;
+//     }
+// };
